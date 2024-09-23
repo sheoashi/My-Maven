4ea22d8214ae95e9a915e8ee687719f021057f4e
shonsisivarghese <shonsisivarghese@192.168.29.167>
2024-09-23 08:06:14 +0530

    third commit

--- My_Maven/src/main/java/Abstraction/AbstractClass.java
+++ My_Maven/src/main/java/Abstraction/AbstractClass.java
@@ -6,7 +6,7 @@ public abstract class AbstractClass
 	public void display()
 	{
 		System.out.println("non abstrct method");
-		System.out.println("first");
+		System.out.println("git test");
 		}
 	}
 
