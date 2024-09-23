234637ac7361fab1c407315c3c86dcd9d5ce42ed
shonsisivarghese <shonsisivarghese@192.168.29.167>
2024-09-23 22:40:34 +0530

    fourth commit

---
+++ My_Maven/src/main/java/exception/LicenseException.java
@@ -0,0 +1,14 @@
+package exception;
+
+public class LicenseException extends Exception 
+{
+	
+	 public LicenseException(String s) 
+	 {
+	 	// TODO Auto-generated constructor stub
+	
+		 super(s);
+	 }
+}
+	
+	 
--- My_Maven/src/main/java/exception/SampleCustomException.java
+++ My_Maven/src/main/java/exception/SampleCustomException.java
@@ -1,9 +1,21 @@
 package exception;
 
 public class SampleCustomException {
+	public void test(int age) throws LicenseException
+	{
+		if (age<18)
+		
+		{
+			 throw new LicenseException("Not eligible for vote");
+		}
+		else
+		{
+			System.out.println("eligible for vote");		}
+	}
+	public static void main(String[] args) throws LicenseException{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SampleCustomException sc =new SampleCustomException();
+		sc.test(24);
 
 	}
 
