7c243a93ce53d9d81d348fe13bcae4874f6e6bd7
shonsisivarghese <shonsisivarghese@192.168.29.167>
2024-09-24 07:28:49 +0530

    fifth commit

---
+++ My_Maven/src/main/java/aggregation/Class1.java
@@ -0,0 +1,12 @@
+package aggregation;
+
+public class Class1 {
+	public void add()
+	{
+		int a=10;
+		int b=20;
+		System.out.println("your answer is :" +(a+b));
+	}
+}
+
+	
---
+++ My_Maven/src/main/java/aggregation/Class2.java
@@ -0,0 +1,20 @@
+package aggregation;
+
+public class Class2 {
+	public void sub()
+	{
+		int a=20;
+		int b=10;
+		System.out.println("your answer:"+(a-b));
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Class1 cs= new Class1();
+		cs.add();
+		Class2 cb= new Class2();
+		cb.sub();
+		
+	}
+
+}
